fb79e2bca9ff4877f2517d09f42856182e0b14a2
Lautaro Barreto <102369702+LautaroBarreto@users.noreply.github.com>
2022-07-29 19:46:03 -0300

    Ciclos
    Agregue un menu con switch como indico la tutora!

--- js/ciclosbarretolautaro.js
+++ js/ciclosbarretolautaro.js
@@ -1,18 +1,43 @@
-alert("Esta es una calculadora virtual, acepta para realizar una funcion")
+alert("Esta es una calculadora virtual, acepta para ver el menu")
 
-let num1 = parseInt(prompt("Ingrese un numero"));
-let num2 = parseInt(prompt("Ingresa otro numero"));
-let operacion = prompt("Elige la operacion: sumar, restar, multiplicar, dividir")
-if (operacion === "sumar") {
-    let resultadosuma = (num1 + num2);
-    alert ("El resultado de la suma es: " + resultadosuma);
-} else if (operacion === "restar") {
-    let resultadoresta = (num1 - num2);
-    alert ("El resultado de la resta es: " + resultadoresta);
-} else if (operacion === "multiplicar") {
-    let resultadomultiplicacion = (num1 * num2);
-    alert ("El resultado de la multiplicacion es: " + resultadomultiplicacion);
-}else if(operacion === "dividir") {
-    let resultadodivision = (num1 / num2);
-    alert ("El resultado de la division es: " + resultadodivision);
+
+let option = prompt(`Ingrese una opcion:
+                        \n1. Realizar una operacion.
+                        \n2. Despedirse de la calculadora virtual.
+                        \n3. Salir del menu.`
+                    )
+
+while(option != '3'){
+    switch (option) {
+        case '1':
+            alert(operacion);
+            break;
+        case '2':
+            alert("Adios Usuario");
+            break;
+        default:
+            break;
+    }
+    option = prompt(`Ingrese una opcion:
+                        \n1. Realizar otra operacion.
+                        \n2. Despedirse de la calculadora virtual.
+                        \n3. Salir del menu`
+                        )
+    let num1 = parseInt(prompt("Ingrese un numero"));
+    let num2 = parseInt(prompt("Ingresa otro numero"));
+    let operacion = prompt("Elige la operacion: sumar, restar, multiplicar, dividir")
+    if (operacion === "sumar") {
+        let resultadosuma = (num1 + num2);
+        alert ("El resultado de la suma es: " + resultadosuma);
+    } else if (operacion === "restar") {
+        let resultadoresta = (num1 - num2);
+        alert ("El resultado de la resta es: " + resultadoresta);
+    } else if (operacion === "multiplicar") {
+        let resultadomultiplicacion = (num1 * num2);
+        alert ("El resultado de la multiplicacion es: " + resultadomultiplicacion);
+    }else if(operacion === "dividir") {
+        let resultadodivision = (num1 / num2);
+        alert ("El resultado de la division es: " + resultadodivision);
+    }
 }
+
