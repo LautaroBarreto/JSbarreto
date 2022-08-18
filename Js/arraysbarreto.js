d696577867871a1b8a817e40619e423889160541
Lautaro Barreto <lautarobarreto92@gmail.com>
2022-08-17 23:09:49 -0300

    Agregue un archivo js para el desafio Dom, eventos

--- Js/arraysbarreto.js
+++ Js/arraysbarreto.js
@@ -2,51 +2,51 @@ alert("Bienvenido a la biblioteca de escalas musicales y acordes");
 
 //escalas mayores
 
-let ordenSostenidos = ("[FA#, DO#, SOL#, RE#, LA#, MI#, SI#]");
-let ordenBemoles = ("[SIb, MIb, LAb, REb, SOLb, DOb, FAb]");
+let ordenSostenidos = "[FA#, DO#, SOL#, RE#, LA#, MI#, SI#]";
+let ordenBemoles = "[SIb, MIb, LAb, REb, SOLb, DOb, FAb]";
 
 //escalas mayores con sostenido
 //reemplazada con array asociativo
 
-let escalaDo = ("[DO, RE, MI, FA, SOL, LA, SI]");
-let escalaSol = ("[SOL, LA, SI, DO, RE, MI, FA#]");
-let escalaRe = ("[RE, MI, FA#, SOL, LA, SI, DO#]");
-let escalaLa = ("[LA, SI, DO#, RE, MI, FA#, SOL#]");
-let escalaMi = ("[MI, FA#, SOL#, LA, SI, DO#, RE#]");
-let escalaSi = ("[SI, DO#, RE#, MI#, FA#, SOL#, LA#]");
-let escalaFaSostenido = ("[FA#, SOL#, LA#, SI, DO#, RE#, MI#[");
-let escalaDoSostenido = ("[DO#, RE#, MI#, FA#, SOL#, LA#, SI#]");
+let escalaDo = "[DO, RE, MI, FA, SOL, LA, SI]";
+let escalaSol = "[SOL, LA, SI, DO, RE, MI, FA#]";
+let escalaRe = "[RE, MI, FA#, SOL, LA, SI, DO#]";
+let escalaLa = "[LA, SI, DO#, RE, MI, FA#, SOL#]";
+let escalaMi = "[MI, FA#, SOL#, LA, SI, DO#, RE#]";
+let escalaSi = "[SI, DO#, RE#, MI#, FA#, SOL#, LA#]";
+let escalaFaSostenido = "[FA#, SOL#, LA#, SI, DO#, RE#, MI#[";
+let escalaDoSostenido = "[DO#, RE#, MI#, FA#, SOL#, LA#, SI#]";
 
 
 
 //escalas mayores con bemoles
-let escalaFa = ("[FA, SOL, LA, SIb, DO, RE, MI]");
-let escalaSib = ("[SIb, DO, RE, MIb, FA, SOL, LA]");
-let escalaMib = ("[MIb, FA, SOL, LAb, SIb, DO, RE]");
-let escalaLab = ("[LAb, SIb, DO, REb, MIb, FA, SOL]");
-let escalaReb = ("[REb, MIb, FA, SOLb, LAb, SIb, DO]");
-let escalaSolb = ("[SOLb, LAb, SIb, DOb, REb, MIb, FA]");
-let escalaDob = ("[DOb, REb, MIb, FAb, SOLb, LAb, SIb]");
+let escalaFa = "[FA, SOL, LA, SIb, DO, RE, MI]";
+let escalaSib = "[SIb, DO, RE, MIb, FA, SOL, LA]";
+let escalaMib = "[MIb, FA, SOL, LAb, SIb, DO, RE]";
+let escalaLab = "[LAb, SIb, DO, REb, MIb, FA, SOL]";
+let escalaReb = "[REb, MIb, FA, SOLb, LAb, SIb, DO]";
+let escalaSolb = "[SOLb, LAb, SIb, DOb, REb, MIb, FA]";
+let escalaDob = "[DOb, REb, MIb, FAb, SOLb, LAb, SIb]";
 
 //escalas menores naturales con sostenidos
-let escalaLaMenor = ("[LA, SI, DO, RE, MI, FA, SOL]");
-let escalaMiMenor = ("[MI, FA#, SOL, LA, SI, DO, RE]");
-let escalaSiMenor = ("[SI, DO#, RE, MI, FA#, SOL, LA]");
-let escalaFaSosMenor = ("[FA#, SOL#, LA, SI, DO#, RE, MI]");
-let escalaDoSosMenor = ("[DO#, RE#, MI, FA#, SOL#, LA, SI]");
-let escalaSolSosMenor = ("[SOL#, LA#, SI, DO#, RE#, MI, FA#]");
-let escalaReSosMenor = ("[RE#, MI#, FA#, SOL#, LA#, SI, DO#]");
-let escalaLaSosMenor = ("[LA#, SI#, DO#, RE#, MI#, FA#, SOL#]");
+let escalaLaMenor = "[LA, SI, DO, RE, MI, FA, SOL]";
+let escalaMiMenor = "[MI, FA#, SOL, LA, SI, DO, RE]";
+let escalaSiMenor = "[SI, DO#, RE, MI, FA#, SOL, LA]";
+let escalaFaSosMenor = "[FA#, SOL#, LA, SI, DO#, RE, MI]";
+let escalaDoSosMenor = "[DO#, RE#, MI, FA#, SOL#, LA, SI]";
+let escalaSolSosMenor = "[SOL#, LA#, SI, DO#, RE#, MI, FA#]";
+let escalaReSosMenor = "[RE#, MI#, FA#, SOL#, LA#, SI, DO#]";
+let escalaLaSosMenor = "[LA#, SI#, DO#, RE#, MI#, FA#, SOL#]";
 
 //escalas menores naturales con bemoles
 
-let escalaReMenor = ("[RE, MI, FA, SOL, LA, SIb, DO]");
-let escalaSolMenor = ("[SOL, LA, SIb, DO, RE, MIb, FA]");
-let escalaDoMenor = ("[DO, RE, MIb, FA, SOL, LAb, SIb]");
-let escalaFaMenor = ("[FA, SOLb, LAb, SIb, DO, REb, MIb]");
-let escalaSibMenor = ("[SIb, DO, REb, MIb, FA, SOLb, LAb]");
-let escalaMibMenor = ("[MIb, FA, SOLb, LAb, SIb, DOb, REb]");
-let escalaLabMenor = ("[LAb, SIb, DOb, REb, MIb, FAb, SOLb]");
+let escalaReMenor = "[RE, MI, FA, SOL, LA, SIb, DO]";
+let escalaSolMenor = "[SOL, LA, SIb, DO, RE, MIb, FA]";
+let escalaDoMenor = "[DO, RE, MIb, FA, SOL, LAb, SIb]";
+let escalaFaMenor = "[FA, SOLb, LAb, SIb, DO, REb, MIb]";
+let escalaSibMenor = "[SIb, DO, REb, MIb, FA, SOLb, LAb]";
+let escalaMibMenor = "[MIb, FA, SOLb, LAb, SIb, DOb, REb]";
+let escalaLabMenor = "[LAb, SIb, DOb, REb, MIb, FAb, SOLb]";
 
 
 //Agrupo las variables creadas con array asociativo, y de esta manera ahorro codigo funcional y puedo utilizar las diferentes escalas por separado mucho mejor.
@@ -80,7 +80,7 @@ while(option != 7) {
             alert("Las Escalas Mayores con Sostenidos son: " + escalasMayConSos);
             break;
         case 4:
-            alert("Las Escalas Mayores con Bemoles son: " + escalaMayConBem);
+            alert("Las Escalas Mayores con Bemoles son: " + escalasMayConBem);
             break;
         case 5:
             alert("Las Escalas Menores Naturales con Sostenidos son: " + escalasMenNatConSos);
